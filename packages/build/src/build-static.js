f5f2a807ea75e7fe9bd0e995497295a1cbc8da87
Le Vivilet <levivlet@pm.me>
2024-10-26 18:42:33 +0200

    build

--- packages/build/src/build-static.js
+++ packages/build/src/build-static.js
@@ -1,7 +1,7 @@
 import { replace } from '@lvce-editor/package-extension'
 import { exportStatic } from '@lvce-editor/shared-process'
-import { cp, readdir, readFile, rm, writeFile } from 'node:fs/promises'
-import path, { join } from 'node:path'
+import { cp } from 'node:fs/promises'
+import path from 'node:path'
 import { root } from './root.js'
 
 await import('./build.js')
@@ -24,29 +24,6 @@ await cp(path.join(root, 'dist2'), path.join(root, 'dist', commitHash, 'extensio
 
 await replace({
   path: path.join(root, 'dist', commitHash, 'config', 'webExtensions.json'),
-  occurrence: 'src/csvViewerMain.ts',
-  replacement: 'dist/csvViewerMain.js',
+  occurrence: 'src/fileSystemFetchMain.ts',
+  replacement: 'dist/fileSystemFetchMain.js',
 })
-await replace({
-  path: path.join(root, 'dist', commitHash, 'config', 'webExtensions.json'),
-  occurrence: '../csv-worker/src/csvWorkerMain.ts',
-  replacement: './csv-worker/dist/csvWorkerMain.js',
-})
-
-const pathPrefix = '/csv-viewer'
-const webViewsPath = join(root, 'dist', commitHash, 'config', 'webViews.json')
-const extensionJsonPath = join(root, 'dist', commitHash, 'extensions', 'builtin.csv-viewer', 'extension.json')
-const extensionJsonContent = await readFile(extensionJsonPath, 'utf8')
-const extensionJson = JSON.parse(extensionJsonContent)
-extensionJson.webViews[0].path = `${commitHash}/extensions/${extensionJson.id}/${extensionJson.webViews[0].path}`
-extensionJson.webViews[0].remotePath = `${pathPrefix}/${commitHash}/extensions/${extensionJson.id}`
-await writeFile(webViewsPath, JSON.stringify(extensionJson.webViews, null, 2) + '\n')
-
-await rm(join(root, 'dist', commitHash, 'playground'), { recursive: true, force: true })
-await cp(join(root, 'packages', 'integration', 'files'), join(root, 'dist', commitHash, 'playground'), {
-  recursive: true,
-})
-
-const dirents = await readdir(join(root, 'dist', commitHash, 'playground'))
-const fileMap = dirents.map((dirent) => `/playground/${dirent}`)
-await writeFile(join(root, 'dist', commitHash, 'config', 'fileMap.json'), JSON.stringify(fileMap, null, 2) + '\n')

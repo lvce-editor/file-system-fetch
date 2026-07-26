958cce5d997220a9a1667356b1c464c73f176216
Le Vivilet <levivilet@pm.me>
2026-07-26 08:34:05 +0000

    feature: migrate to npm workspaces

--- scripts/update-dependencies.sh
+++ scripts/update-dependencies.sh
@@ -16,21 +16,14 @@ fi
 
 function updateDependencies {
   echo "updating dependencies..."
-  OUTPUT=`ncu -u -x @types/node -x typescript`
-  SUB='All dependencies match the latest package versions'
-  if [[ "$OUTPUT" == *"$SUB"* ]]; then
-    echo "$OUTPUT"
-  else
-    rm -rf node_modules package-lock.json dist
-    npm install
-  fi
+  ncu -u -x @types/node -x typescript
 }
 
-                                    updateDependencies &&
-cd packages/build                && updateDependencies && cd ../../ &&
-cd packages/extension            && updateDependencies && cd ../../ &&
-cd packages/integration          && updateDependencies && cd ../../ &&
-cd packages/csv-worker           && updateDependencies && cd ../../ &&
+updateDependencies &&
+for packagePath in packages/*; do
+  (cd "$packagePath" && updateDependencies) || exit 1
+done
+npm install &&
 
 echo "Great Success!"
 
